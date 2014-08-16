9ec751dca072f4f062a9e13d6fa747f5df849f71
Dana <dana9107@gmail.com>
2014-08-16 10:57:35 +0000

    my twitte

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+
+  get 'users/show'
+  
+  match '/signup', to: "users#new", via: :get
+  root 'static_pages#home'
+  match '/about', to: 'static_pages#about', via: :get
+  match '/help', to: 'static_pages#help', via: :get
+  match '/contact', to: 'static_pages#contact', via: :get
+
   resources :microposts
 
   resources :users
