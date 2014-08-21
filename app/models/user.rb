530483a2170a5fb1362e82fd95777fc4117f4f04
Dana <dana9107@gmail.com>
2014-08-21 09:16:40 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,35 @@
 class User < ActiveRecord::Base
+ has_many :microposts, dependent: :destroy
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
+  before_create :create_remember_token
+  
+  before_save {self.email = email.downcase}
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+  
+   def feed
+    # Это предварительное решение. См. полную реализацию в "Following users".
+    Micropost.where("user_id = ?", id)
+  end
+  
+     def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-
-
+  resources :users
+  resources :sessions,   only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
+ 
   get 'users/show'
   
   match '/signup', to: "users#new", via: :get
+  
+  match '/signin', to: "sessions#new", via: :get
+  match '/signout', to: "sessions#destroy", via: :delete
+  
   root 'static_pages#home'
+  
   match '/about', to: 'static_pages#about', via: :get
   match '/help', to: 'static_pages#help', via: :get
   match '/contact', to: 'static_pages#contact', via: :get
@@ -12,6 +19,9 @@ Rails.application.routes.draw do
   resources :microposts
 
   resources :users
+  
+  resources :sessions, only: [:new, :create, :destroy]
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
